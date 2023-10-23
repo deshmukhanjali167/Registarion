d26402fced05df6bc2ebcfad933ca8a7f2e12d73
anjali-dev <deshmukhanjali167@gmail.com>
2023-10-23 10:24:21 +0530

    studentForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './page/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page/registration/registration.component.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent  {
+  fullname: string = '';
+  studentfrom: any = {
+    firstName: "",
+    middleName: "",
+    lastName: "",
+    contactno: "",
+    email: "",
+    dob: "",
+    age: "",
+    licence: "",
+    eduction: "",
+    highestedu: "",
+    highpermark: "",
+    highpassingyear: "",
+    hsc: "",
+    hscpassing: "",
+    degfyear: "",
+    degsyear: "",
+    degtyear: "",
+    degpassing: "",
+    startyear: "",
+    endyear: ""
+
+  }
+  createfullname() {
+    this.fullname = this.studentfrom.firstName + " " + this.studentfrom.middleName + " " + this.studentfrom.lastName
+
+  }
+
+  dobDate() {
+    const selectyear = new Date(this.studentfrom.dob);
+    const dob = selectyear.getFullYear();
+    const curerentyear = new Date().getFullYear();
+    this.studentfrom.age = curerentyear - dob
+  }
+
+  eductionYear() {
+    const startYear = new Date(this.studentfrom.startyear).getFullYear();
+    const endYear = new Date(this.studentfrom.endyear).getFullYear();
+    const differ = endYear - startYear;
+    if (differ != 3) {
+      alert("You are not eligible for this course")
+    }
+
+  }
+  onReset() {
+    this.studentfrom = {
+      firstName: "",
+      middleName: "",
+      lastName: "",
+      contactno: "",
+      email: "",
+      dob: "",
+      age: "",
+      licence: "",
+      eduction: "",
+      highestedu: "",
+      highpermark: "",
+      highpassingyear: "",
+      hsc: "",
+      hscpassing: "",
+      degfyear: "",
+      degsyear: "",
+      degtyear: "",
+      degpassing: "",
+      startyear: "",
+      endyear: ""
+
+    }
+
+  }
+
+}
+
